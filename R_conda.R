e2ac4742d796abba25002e156fd9737ee59c2982
Jay Chow <s2600569@bioinfmsc7.bio.ed.ac.uk>
2024-07-29 15:54:31 +0100

    added R pkg installation script

---
+++ R_conda.R
@@ -0,0 +1,57 @@
+#!/localdisk/home/s2600569/project/envs/benchdeconv_env/bin/R
+
+
+
+
+
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
+
+
+
+install.packages('devtools')#
+library(devtools)
+
+
+install_version("Matrix", version = "1.6.5", repos = "http://cran.us.r-project.org")
+library(Matrix)
+
+
+
+if (!require("BiocManager", quietly = TRUE))#
+	    install.packages("BiocManager")#
+
+BiocManager::install("DropletUtils")#
+
+
+install.packages('SeuratObject')#
+install.packages('Seurat')#
+#install.packages("igraph")
+install.packages(c("BPCells", "presto", "glmGamPoi"))
+
+BiocManager::install("DropletUtils")
+BiocManager::install("SpatialExperiment")#
+BiocManager::install("SingleCellExperiment")#
+
+
+install.packages("pak")#
+pak::pkg_install("omnideconv/spacedeconv", dependencies=TRUE)
+
+
+
+#install.packages("spacexr") # RCTD
+#devtools::install_github('YMa-lab/CARD') # CARD
+#install.packages("SPOTlight") # SPOTlight
+
+BiocManager::install("scran") # spatialdwls
+BiocManager::install("BiocParallel")# spatialdwls
+BiocManager::install("genefilter")# spatialdwls
+BiocManager::install("DESeq2")# spatialdwls
+library('devtools')# spatialdwls
+devtools::install_github('edsgard/trendsceek')# spatialdwls
+install.packages('multinet')# spatialdwls
+install.packages('FactoMineR')# spatialdwls
+
+BiocManager::install("SpatialDecon")# spatialdecon
+
+install.packages("philentropy")#jsd stats 
+
