2efab1d5f8e88a024cbcf6ede7ad57e1e29740e8
Jay Chow <s2600569@bioinfmsc7.bio.ed.ac.uk>
2024-07-30 04:56:58 +0100

    added synthspot to R installation scripot

--- R_conda.R
+++ R_conda.R
@@ -1,42 +1,30 @@
-#!/localdisk/home/s2600569/project/envs/benchdeconv_env/bin/R
-
-
-
-
+#!/exports/eddie/scratch/s2600569/envs/benchdeconv_env/bin/R
 
 options(repos = c(CRAN = "https://cloud.r-project.org/"))
 
+if (!require("BiocManager", quietly = TRUE))#
+	    install.packages("BiocManager")
 
-
-install.packages('devtools')#
+install.packages('devtools')
 library(devtools)
 
-
 install_version("Matrix", version = "1.6.5", repos = "http://cran.us.r-project.org")
-library(Matrix)
 
 
+BiocManager::install("DropletUtils")
 
-if (!require("BiocManager", quietly = TRUE))#
-	    install.packages("BiocManager")#
-
-BiocManager::install("DropletUtils")#
-
-
-install.packages('SeuratObject')#
-install.packages('Seurat')#
-#install.packages("igraph")
+install.packages('SeuratObject')
+install.packages('Seurat')
+install.packages("igraph")
 install.packages(c("BPCells", "presto", "glmGamPoi"))
 
 BiocManager::install("DropletUtils")
-BiocManager::install("SpatialExperiment")#
-BiocManager::install("SingleCellExperiment")#
+BiocManager::install("SpatialExperiment")
+BiocManager::install("SingleCellExperiment")
 
 
-install.packages("pak")#
+install.packages("pak")
 pak::pkg_install("omnideconv/spacedeconv", dependencies=TRUE)
-
-
 
 #install.packages("spacexr") # RCTD
 #devtools::install_github('YMa-lab/CARD') # CARD
@@ -46,7 +34,6 @@ BiocManager::install("scran") # spatialdwls
 BiocManager::install("BiocParallel")# spatialdwls
 BiocManager::install("genefilter")# spatialdwls
 BiocManager::install("DESeq2")# spatialdwls
-library('devtools')# spatialdwls
 devtools::install_github('edsgard/trendsceek')# spatialdwls
 install.packages('multinet')# spatialdwls
 install.packages('FactoMineR')# spatialdwls
